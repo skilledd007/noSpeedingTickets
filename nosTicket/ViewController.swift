30fd9fc3e75e29468f5ea208dbe71ea8c146e8fd
Amrit Mahendrarajah <amritm3000@gmail.com>
2022-07-24 17:03:22 -0600

    can get correct speed for Location now.

--- nosTicket/ViewController.swift
+++ nosTicket/ViewController.swift
@@ -7,14 +7,23 @@
 
 import UIKit
 import AVFoundation
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate,SpeedManagerDelegate {
 
     var audioPlayer = AVAudioPlayer()
+    let locationManager = CLLocationManager()
+    var speedManager = SpeedManager()
+    
+    @IBOutlet weak var speedLimitLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        locationManager.delegate = self
         // Do any additional setup after loading the view.
+        speedManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
         let sound = Bundle.main.path(forResource: "alarm", ofType: "mp3")
         do {
             try AVAudioSession.sharedInstance().setCategory(.playback)
@@ -26,37 +35,36 @@ class ViewController: UIViewController {
         } catch let error {
             print(error.localizedDescription)
         }
-        var urlString = "https://routematching.hereapi.com/v8/match/routelinks?apikey=U5Mo7l-eKI5hX66VE7PSuLC1RaCu5BPAxcihb68q6G0&waypoint0=51.12326,-114.13433&waypoint1=51.12326,-114.13433&mode=fastest;car&routeMatch=1&attributes=SPEED_LIMITS_FCn(*)"
-        if let url = URL(string: urlString) {
-            //2. Create a URLSession
-            let session = URLSession(configuration: .default)
-            //3. Give the session a task
-            let task = session.dataTask(with: url) { (data,response,error) in
-                if error != nil {
-                    print(error!)
-                    return
-                }
-                
-                if let safeData = data {
-                    let decoder = JSONDecoder()
-                    do {
-                            let responseModel = try decoder.decode(Json4Swift_Base.self, from: data!)
-                        //print(responseModel.response?.route[0].leg[0].link[0].attributes.SPEED_LIMITS_FCN[0].FROM_REF_SPEED_LIMIT)
-                        print(responseModel.response?.route![0].leg![0].link![0].attributes?.sPEED_LIMITS_FCN![0].fROM_REF_SPEED_LIMIT)
-                        
-                    } catch {
-                        print(error)
-                    }
-                }
-            }
-            //4. Start the task
-            task.resume()
-        }
+        
         
     }
 
     @IBAction func playSoundPressed(_ sender: UIButton) {
         audioPlayer.play()
+    }
+    
+   
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            let lat = location.coordinate.latitude
+            let lon = location.coordinate.longitude
+            speedManager.getSpeedLimit(lat: lat, lon: lon)
+           
+            print("Latitude: \(lat)")
+            print("Longitude: \(lon)")
+            
+        }
+       
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+    func didUpdateSpeed(speed: String) {
+        
+            DispatchQueue.main.async {
+                self.speedLimitLabel.text = speed
+                print(speed)
+            }
     }
     
 }
